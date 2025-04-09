a169a5aade64d449ac531e71b6d7c2ffb8dd54d5
Julia Shestakova <greenroach@gmail.com>
2025-04-09 09:32:46 +0300

    Add react-aria-components and setup aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: "@tokens",
+        replacement: fileURLToPath(
+          new URL("./src/components/tokens", import.meta.url),
+        ),
+      },
+    ],
+  },
 });
